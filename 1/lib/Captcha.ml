8b144165e3d42a1c6d89858553b15c9edf366153
Thomas Coopman <thomas.coopman@gmail.com>
2017-12-01 21:38:16 +0100

    star 2

--- 1/lib/Captcha.ml
+++ 1/lib/Captcha.ml
@@ -19,3 +19,13 @@ let sum_captcha input =
         | hd::snd::tl -> sum (snd::tl)
     in
     sum make_circular
+
+let sum_captcha_2 input =
+    let input = Array.of_list input in
+    let half = Array.length input/2 in
+    let matching i = (i + half) mod (Array.length input) in
+    CCArray.foldi (fun acc i val' -> 
+        match input.(matching i) with
+        | x when x = val' -> acc + val'
+        | _ -> acc
+    ) 0 input
