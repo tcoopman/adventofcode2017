d73ec1f990276ab4feda27d995a5803e46aa5b07
Thomas Coopman <thomas.coopman@gmail.com>
2017-12-02 10:38:22 +0100

    day 2 star 2

--- lib/day2.ml
+++ lib/day2.ml
@@ -14,3 +14,18 @@ let spreadsheet input =
     let max l = CCList.fold_left CCInt.max min_int l in
     let min l = CCList.fold_left CCInt.min max_int l in
     CCList.fold_left (fun acc val' -> acc + (max val'-min val')) 0 input
+
+let spreadsheet2 input = 
+    let rec rowval l = 
+        CCList.find_mapi (fun i number1 -> 
+            CCList.foldi (fun acc j number2 -> 
+                match acc with
+                | Some _ -> acc
+                | None when i = j -> acc
+                | None ->
+                    if (number1 mod number2) = 0 then Some (number1 / number2) else None
+            ) None l
+        ) l
+        |> CCOpt.get_exn
+    in
+    CCList.fold_left (fun acc val' -> acc + (rowval val')) 0 input
