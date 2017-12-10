662ae90207e7d1fab964b98c3413eb677df2d258
Thomas Coopman <thomas.coopman@gmail.com>
2017-12-10 16:49:58 +0100

    day 10 star 2

--- lib/day10.ml
+++ lib/day10.ml
@@ -24,17 +24,44 @@ let reverse_part position length l =
     |> List.iter (fun (i, v) -> Array.set arr i v);
     Array.to_list arr
 
+let rec hash' list lengths position skip_size =
+    match lengths with
+    | [] -> (list, position, skip_size)
+    | length :: tl ->
+        let list = reverse_part position length list in
+        hash' list tl (position + length + skip_size) (skip_size + 1)
+
 let hash list_length lengths =
     let list = CCList.range' 0 list_length in
-    let rec hash' list lengths position skip_size =
-        match lengths with
-        | [] -> list
-        | length :: tl ->
-            let list = reverse_part position length list in
-            hash' list tl (position + length + skip_size) (skip_size + 1)
-    in
-    hash' list lengths 0 0
+    let (output, _, _) = hash' list lengths 0 0 in
+    output
 
 let multiply = function
     | hd1 :: hd2 :: _ -> hd1 * hd2
     | _ -> min_int
+
+let convert_to_input_list input =
+    input |> CCString.to_list |> List.map int_of_char |> (fun l -> List.concat [l; [17;31;73;47;23]])
+
+let rec sparse_hash rounds input = 
+    let list = CCList.range' 0 256 in
+    let rec round r input l position skip_size =
+        match r with
+        | 0 -> l
+        | _ -> 
+            let (l, position, skip_size) = hash' l input position skip_size in
+            round (r-1) input l position skip_size
+    in
+    round rounds input list 0 0
+
+let dense_hash sparse =
+    CCList.sublists_of_len 16 sparse
+    |> List.map (List.fold_left (fun acc i -> acc lxor i) 0)
+    |> List.map (Printf.sprintf "%02x")
+    |> String.concat ""
+
+let knot_hash input =
+    input
+    |> convert_to_input_list
+    |> sparse_hash 64
+    |> dense_hash
--- tests/day10.ml
+++ tests/day10.ml
@@ -11,5 +11,19 @@ let test_set_star1 = [
   "Test multiply", `Quick, test_multiple 256 puzzle_input_lengths 23715;
 ]
 
+let puzzle_input = "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"
+
+let test_convert_to_input_list () =
+    Alcotest.(check (list int)) "" (convert_to_input_list "1,2,3") [49;44;50;44;51;17;31;73;47;23]
+
+let test_convert_to_knop_hash () =
+    Alcotest.(check string) "" (knot_hash "") "a2582a3a0e66e6e86e3812dcb672a272"
+
+let test_puzzle () =
+    Alcotest.(check string) "" (knot_hash puzzle_input) "541dc3180fd4b72881e39cf925a50253"
+
 let test_set_star2 = [
+    "Input list", `Quick, test_convert_to_input_list;
+    "Test knot hash", `Quick, test_convert_to_knop_hash;
+    "Test puzzle", `Quick, test_puzzle;
 ]
